e99983e2adad0938109f2ec54f41ed9f52a32303
dioramashvili <dioramashvili@gmail.com>
2026-04-30 14:00:27 +0400

    feat: MessageParser class created

--- src/ge/bsb/ops/statistics/parser/MessageParser.java
+++ src/ge/bsb/ops/statistics/parser/MessageParser.java
@@ -1,8 +1,23 @@
 package ge.bsb.ops.statistics.parser;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ge.bsb.ops.statistics.model.Transaction;
 
+import java.time.LocalDate;
+
 public class MessageParser {
-//    public Transaction parse(String body){
-//    }
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public Transaction parse(String body) throws Exception {
+        JsonNode root = objectMapper.readTree(body);
+
+        Transaction transaction = new Transaction();
+        transaction.setDebitCustomerId(root.get("debitCustomerId").asInt());
+        transaction.setCreditCustomerId(root.get("creditCustomerId").asInt());
+        transaction.setChannelId(root.get("channelId").asInt());
+        transaction.setDate(LocalDate.parse(root.get("date").asText()));
+
+        return transaction;
+    }
 }
