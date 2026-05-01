058daeb216022ef44434e7fb92b81e0243e0b66e
dioramashvili <dioramashvili@gmail.com>
2026-05-01 13:48:04 +0400

    fix: explicitly load MSSQL JDBC driver and add classpath fallback for config.properties

--- src/ge/bsb/ops/statistics/repository/DatabaseConnection.java
+++ src/ge/bsb/ops/statistics/repository/DatabaseConnection.java
@@ -1,14 +1,34 @@
 package ge.bsb.ops.statistics.repository;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnection {
-    static Properties props = new Properties();
+    private static final String URL;
 
-    private static final String URL =props.getProperty("sqlserver.url");
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MSSQL JDBC Driver not found", e);
+        }
+
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            try {
+                props.load(DatabaseConnection.class.getClassLoader().getResourceAsStream("config.properties"));
+            } catch (IOException ex) {
+                throw new RuntimeException("Could not load config.properties", ex);
+            }
+        }
+        URL = props.getProperty("sqlserver.url");
+    }
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
