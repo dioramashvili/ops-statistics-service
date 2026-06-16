2ab1304fd859085773e0d0181e32c6d19269d5c9
dioramashvili <dioramashvili@gmail.com>
2026-06-16 16:36:07 +0400

    Replace MERGE with concurrency-safe update/insert logic

--- src/main/java/ge/bsb/ops/statistics/repository/StatisticsRepository.java
+++ src/main/java/ge/bsb/ops/statistics/repository/StatisticsRepository.java
@@ -2,6 +2,7 @@ package ge.bsb.ops.statistics.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -18,32 +19,52 @@ public class StatisticsRepository {
     }
 
     public void increment(String debitSegment, String creditSegment, int channelId, LocalDate date) {
-        jdbcTemplate.update(
-                """
-                        MERGE basis.OPS_SEGMENT_STATISTICS_DAVIT AS target
-                        USING (VALUES (?, ?, ?, ?)) AS source (debit_segment, credit_segment, channel_id, doc_date)
-                        ON target.debit_segment = source.debit_segment
-                        AND target.credit_segment = source.credit_segment
-                        AND target.channel_id = source.channel_id
-                        AND target.doc_date = source.doc_date
-                        WHEN MATCHED THEN
-                            UPDATE SET op_count = target.op_count + 1
-                        WHEN NOT MATCHED THEN
-                            INSERT (debit_segment, credit_segment, channel_id, doc_date, op_count)
-                            VALUES (?, ?, ?, ?, 1);
-                        """,
-                debitSegment,
-                creditSegment,
-                channelId,
-                java.sql.Date.valueOf(date),
-
-                debitSegment,
-                creditSegment,
-                channelId,
-                java.sql.Date.valueOf(date)
-        );
-        log.info("Statistics incremented - debit: {}, credit: {}, channel: {}, date: {}",
-                debitSegment, creditSegment, channelId, date);
+        try {
+            jdbcTemplate.update(
+                    """
+                            BEGIN TRY
+                                UPDATE basis.OPS_SEGMENT_STATISTICS_DAVIT
+                                SET op_count = op_count + 1
+                                WHERE debit_segment  = ?
+                                  AND credit_segment = ?
+                                  AND channel_id     = ?
+                                  AND doc_date       = ?;
+                            
+                                IF @@ROWCOUNT = 0
+                                BEGIN
+                                    INSERT INTO basis.OPS_SEGMENT_STATISTICS_DAVIT
+                                        (debit_segment, credit_segment, channel_id, doc_date, op_count)
+                                    VALUES (?, ?, ?, ?, 1);
+                                END
+                            END TRY
+                            BEGIN CATCH
+                                IF ERROR_NUMBER() IN (2601, 2627)
+                                BEGIN
+                                    UPDATE basis.OPS_SEGMENT_STATISTICS_DAVIT
+                                    SET op_count = op_count + 1
+                                    WHERE debit_segment  = ?
+                                      AND credit_segment = ?
+                                      AND channel_id     = ?
+                                      AND doc_date       = ?;
+                                END
+                                ELSE THROW;
+                            END CATCH
+                            """,
+                    // UPDATE
+                    debitSegment, creditSegment, channelId, java.sql.Date.valueOf(date),
+                    // INSERT
+                    debitSegment, creditSegment, channelId, java.sql.Date.valueOf(date),
+                    // CATCH UPDATE
+                    debitSegment, creditSegment, channelId, java.sql.Date.valueOf(date)
+            );
+            log.info("Statistics incremented - debit: {}, credit: {}, channel: {}, date: {}",
+                    debitSegment, creditSegment, channelId, date);
+        } catch (
+                DataAccessException e) {
+            log.error("Failed to increment segment statistics for debitSegment={}, creditSegment={}, channelId={}, date={}",
+                    debitSegment, creditSegment, channelId, date, e);
+            throw e;
+        }
     }
 
     public void decrement(String debitSegment, String creditSegment, int channelId, LocalDate date) {
